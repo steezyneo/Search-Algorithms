701792cf6ba6f876325e3d4845831d66ee61d465
Steezy <104679786+steezyneo@users.noreply.github.com>
2022-09-10 00:43:53 +0530

    Update Bsearch.c

--- Bsearch.c
+++ Bsearch.c
@@ -1,31 +1,26 @@
 #include<stdio.h>
-int bsearch(int a[], int low, int high, int key);
 void main(){
-	int a[5], key, pos, i, n;
-	printf("Enter size of array: ");
+	int target, low=0, mid, high, a[10], i, found = 0, n;
+	printf("Enter number of elements: ");
 	scanf("%d", &n);
-	printf("Enter %d elements in ascending or descending order:\n", n);
+	printf("Enter elements in ascending order: ");
 	for(i=0; i<n; i++)
 	scanf("%d", &a[i]);
-	printf("Enter an element that is to be scanned: ");
-	scanf("%d", &key);
-	pos = bsearch(a, 0, n-1, key);
-	if(pos == -1)
-	printf("Sorry, target item was not found");
-	else
-	printf("\n Target item was found at index : %d", pos);
-}
-
-int bsearch(int a[], int low, int high, int key){
-	int mid;
+	printf("Enter an element to be searched: ");
+	scanf("%d", &target);
+	high = n-1;
 	while(low<=high){
 		mid = (low+high)/2;
-		if(key == a[mid])
-		return mid;
-		else if(key < a[mid])
-		high = mid-1;
-		else
-		low = mid+1;
+		if(target < a[mid])
+			high = mid-1;
+		else if(target > a[mid])
+			low = mid+1;
+		else{
+			printf("%d found at index = %d", target, i);
+			found = 1;
+			break;
+		}
 	}
-	return -1;
+	if(!found)
+		printf("%d is not found", target);
 }
